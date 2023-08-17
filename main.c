2437db9d75470c6af68090158e52e8506718a2a4
carlfrank <carl-frank7@hotmail.com>
2023-08-17 13:01:01 -0400

    main.c

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "simple_shell.h"
+
+/**
+ * main - gets user input and calls
+ * function(s) to perform commmand
+ * @ac: argument count
+ * @av: argument vector (array)
+ * Return: 0. 
+ */
+
+int main(int ac, char **av)
+
+{
+	char *line;
+	char **args;
+	int status;
+	bool running = true;
+
+	(void)ac;
+	(void)av;
+
+	while (running)
+	{
+		prompt();
+		line = read_input();
+
+		if (line == NULL)
+			break;
+
+		args = tokenize(line);
+		status = execute(args);
+		free(line);
+		free_array(args);
+
+		if (status == -1)
+		{
+			running = false;
+		}
+	}
+	return (0);
+
+}
